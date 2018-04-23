e4e122230da2b80113fdb066b6d2e2471cbb8b56
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SpecifySVGImagesManually/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Core.Native;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace SpecifySVGImagesManually {    
+    public partial class MainWindow{
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = new List<String>() { "Images/First.svg", "Images/Last.svg" };
+        }
+    }
+    public class SvgImageSourceConverterExtension : MarkupExtension, IValueConverter {
+        class UriStreamHelper : SvgImageSourceExtension {
+            public static Stream GetStream(Uri uri) {
+                return CreateRequestAndGetResponseStream(uri);
+            }
+        }
+
+        readonly Uri baseUri;
+        readonly UriTypeConverter uriConverter;
+        
+        public SvgImageSourceConverterExtension() : this(null) { }
+        public SvgImageSourceConverterExtension(Uri baseUri) { 
+            this.baseUri = baseUri;
+            uriConverter = new UriTypeConverter();
+        }
+        
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return new SvgImageSourceConverterExtension((serviceProvider.GetService(typeof(IUriContext)) as IUriContext).BaseUri);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var uri = uriConverter.ConvertFrom(value) as Uri;
+            if (uri == null)
+                return null;
+            var absoluteUri = uri.IsAbsoluteUri ? uri : new Uri(baseUri, uri);
+            using(var stream = UriStreamHelper.GetStream(absoluteUri)) {
+                object unused = null;
+                var image = SvgImageHelper.GetOrCreateSvgImage(stream, ref unused);
+                return WpfSvgRenderer.CreateImageSource(image, 1d, null, null, true);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to specify SVG images manually
+
+
+If you have ViewModel with string paths to *.svg files, you can create the <a href="https://msdn.microsoft.com/library/system.windows.data.ivalueconverter">IValueConverter</a> that converts these paths to the <a href="https://msdn.microsoft.com/library/system.windows.media.imagesource">ImageSource</a> objects.<br><br>Refer to the <a href="https://documentation.devexpress.com/WPF/114013/Common-Concepts/Images">Images</a> topic to learn more.
+
+<br/>
+
+
