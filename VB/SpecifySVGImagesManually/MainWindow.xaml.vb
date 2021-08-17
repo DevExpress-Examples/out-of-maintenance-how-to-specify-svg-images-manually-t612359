9f9560363275634f9b9bbf88a89658b9e1077bb2
DevExpressExampleBot <fakeEmail>
2021-08-17 13:39:17 +0300

    Source auto update [skip ci]

--- VB/SpecifySVGImagesManually/MainWindow.xaml.vb
+++ VB/SpecifySVGImagesManually/MainWindow.xaml.vb
@@ -1,7 +1,9 @@
-﻿Imports DevExpress.Xpf.Core.Native
+﻿Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Core.Native
 Imports System
-Imports System.Collections.ObjectModel
+Imports System.Collections.Generic
 Imports System.Globalization
+Imports System.IO
 Imports System.Windows.Data
 Imports System.Windows.Markup
 
@@ -9,7 +11,7 @@ Namespace SpecifySVGImagesManually
 	Partial Public Class MainWindow
 		Public Sub New()
 			InitializeComponent()
-			DataContext = New ObservableCollection(Of String)() From {"Images/First.svg", "Images/Last.svg"}
+			DataContext = New List(Of String)() From {"Images/First.svg", "Images/Last.svg"}
 		End Sub
 	End Class
 	Public Class SvgImageSourceConverterExtension
@@ -30,14 +32,14 @@ Namespace SpecifySVGImagesManually
 		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
 			Return New SvgImageSourceConverterExtension((TryCast(serviceProvider.GetService(GetType(IUriContext)), IUriContext)).BaseUri)
 		End Function
-
 		Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
 			Dim uri As Uri = TryCast(uriConverter.ConvertFrom(value), Uri)
 			If uri Is Nothing Then
 				Return Nothing
 			End If
 			Dim absoluteUri = If(uri.IsAbsoluteUri, uri, New Uri(baseUri, uri))
-			Return WpfSvgRenderer.CreateImageSource(absoluteUri)
+			Dim image = SvgImageHelper.CreateImage(absoluteUri)
+			Return WpfSvgRenderer.CreateImageSource(image, 1R, Nothing, Nothing, True)
 		End Function
 
 		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
