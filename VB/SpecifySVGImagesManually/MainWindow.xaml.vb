70c5e5ca51e860d172b505cffea6f2e2970d9446
DevExpressExampleBot <fakeEmail>
2022-12-17 13:06:02 +0400

    Source auto update [skip ci]

--- VB/SpecifySVGImagesManually/MainWindow.xaml.vb
+++ VB/SpecifySVGImagesManually/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports DevExpress.Xpf.Core.Native
+Imports DevExpress.Xpf.Core.Native
 Imports System
 Imports System.Collections.ObjectModel
 Imports System.Globalization
@@ -6,42 +6,45 @@ Imports System.Windows.Data
 Imports System.Windows.Markup
 
 Namespace SpecifySVGImagesManually
-	Partial Public Class MainWindow
-		Public Sub New()
-			InitializeComponent()
-			DataContext = New ObservableCollection(Of String)() From {"Images/First.svg", "Images/Last.svg"}
-		End Sub
-	End Class
-	Public Class SvgImageSourceConverterExtension
-		Inherits MarkupExtension
-		Implements IValueConverter
 
-		Private ReadOnly baseUri As Uri
-		Private ReadOnly uriConverter As UriTypeConverter
+    Public Partial Class MainWindow
 
-		Public Sub New()
-			Me.New(Nothing)
-		End Sub
-		Public Sub New(ByVal baseUri As Uri)
-			Me.baseUri = baseUri
-			uriConverter = New UriTypeConverter()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.DataContext = New ObservableCollection(Of String)() From {"Images/First.svg", "Images/Last.svg"}
+        End Sub
+    End Class
 
-		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-			Return New SvgImageSourceConverterExtension((TryCast(serviceProvider.GetService(GetType(IUriContext)), IUriContext)).BaseUri)
-		End Function
+    Public Class SvgImageSourceConverterExtension
+        Inherits MarkupExtension
+        Implements IValueConverter
 
-		Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
-			Dim uri As Uri = TryCast(uriConverter.ConvertFrom(value), Uri)
-			If uri Is Nothing Then
-				Return Nothing
-			End If
-			Dim absoluteUri = If(uri.IsAbsoluteUri, uri, New Uri(baseUri, uri))
-			Return WpfSvgRenderer.CreateImageSource(absoluteUri)
-		End Function
+        Private ReadOnly baseUri As Uri
 
-		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
-			Throw New NotImplementedException()
-		End Function
-	End Class
+        Private ReadOnly uriConverter As UriTypeConverter
+
+        Public Sub New()
+            Me.New(Nothing)
+        End Sub
+
+        Public Sub New(ByVal baseUri As Uri)
+            Me.baseUri = baseUri
+            uriConverter = New UriTypeConverter()
+        End Sub
+
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return New SvgImageSourceConverterExtension(TryCast(serviceProvider.GetService(GetType(IUriContext)), IUriContext).BaseUri)
+        End Function
+
+        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+            Dim uri = TryCast(uriConverter.ConvertFrom(value), Uri)
+            If uri Is Nothing Then Return Nothing
+            Dim absoluteUri = If(uri.IsAbsoluteUri, uri, New Uri(baseUri, uri))
+            Return WpfSvgRenderer.CreateImageSource(absoluteUri)
+        End Function
+
+        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Throw New NotImplementedException()
+        End Function
+    End Class
 End Namespace
